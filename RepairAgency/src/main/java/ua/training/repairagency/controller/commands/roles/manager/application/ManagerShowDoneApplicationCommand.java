3a2ff0be35ae0ed4ff6f551c0273d1fd4166433f
Vitalii Hrushyn <vhrushyn@gmail.com>
2018-05-16 21:24:44 +0300

    useless classes deleted

---
+++ RepairAgency/src/main/java/ua/training/repairagency/controller/commands/roles/manager/application/ManagerShowDoneApplicationCommand.java
@@ -0,0 +1,44 @@
+package ua.training.repairagency.controller.commands.roles.manager.application;
+
+import ua.training.repairagency.controller.constants.URL;
+import ua.training.repairagency.model.entities.application.Application;
+
+import static ua.training.repairagency.controller.constants.AttributeOrParam.DONE_APPLICATION;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import ua.training.repairagency.controller.commands.Command;
+import ua.training.repairagency.controller.commands.roles.abstracts.application.AbstractShowListApplicationCommand;
+@Deprecated
+public class ManagerShowDoneApplicationCommand extends AbstractShowListApplicationCommand {
+
+	private static String path = URL.MANAGER_APPLICATIONS_DONE_PATH;
+
+	public ManagerShowDoneApplicationCommand(Map<String, Command> commands) {
+		super(path , commands);
+	}
+
+	@Override
+	protected String getApplicationPage() {
+		return URL.COMMON_APPLICATION_DONE_PAGE; 
+	}
+
+	@Override
+	protected List<Application> getApplications(HttpServletRequest request, int pageNumber) {
+		return serviceFactory
+				.createApplicationService()
+				.getAllByStatuses(	  pageNumber
+									, DONE_APPLICATION
+									, null
+									, null);
+	}
+	
+	@Override
+	protected String getPath() {
+		return path;
+	}
+	
+}
