6a4bc98a797bc81f247f559162c36235dafe9d37
Vitalii Hrushyn <vhrushyn@gmail.com>
2018-05-16 12:26:22 +0300

    customer creation new application fixed

--- RepairAgency/src/main/java/ua/training/repairagency/controller/commands/roles/abstracts/application/AbstractCreateApplicationCommand.java
+++ RepairAgency/src/main/java/ua/training/repairagency/controller/commands/roles/abstracts/application/AbstractCreateApplicationCommand.java
@@ -27,7 +27,7 @@ public abstract class AbstractCreateApplicationCommand extends AbstractCommand {
 		infoMessages = new ArrayList<>();
 		errorMessages = new ArrayList<>();
 		
-		if (CommandUtils.isRequestContainsParam(request, DESCRIPTION)) {
+		if (!CommandUtils.isRequestContainsParam(request, DESCRIPTION)) {
 			return getApplicationCreatePage();
 		}
 		
