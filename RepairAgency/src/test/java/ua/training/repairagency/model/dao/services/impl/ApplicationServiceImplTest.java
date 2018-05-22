d0602831d2122c562d5c6bfe7656e201bd1e29bd
Vitalii Hrushyn <vhrushyn@gmail.com>
2018-05-22 13:55:41 +0300

    tests stub added

---
+++ RepairAgency/src/test/java/ua/training/repairagency/model/dao/services/impl/ApplicationServiceImplTest.java
@@ -0,0 +1,54 @@
+package ua.training.repairagency.model.dao.services.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ApplicationServiceImplTest {
+
+	@Test
+	public void testGetNumberOfPagesByQuery() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAllByUserId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAllByStatuses() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInsert() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAllByCustomerIdAndStatuses() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAllByWorkmanIdAndStatuses() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ RepairAgency/src/test/java/ua/training/repairagency/model/dao/services/impl/TestimonialServiceImplTest.java
@@ -0,0 +1,24 @@
+package ua.training.repairagency.model.dao.services.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestimonialServiceImplTest {
+
+	@Test
+	public void testGetById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInsert() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDelete() {
+		fail("Not yet implemented");
+	}
+
+}
